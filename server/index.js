fab5ad32cda2e840e0180132be78fa25245e1712
Anthony <anthony.cersosimo@gmail.com>
2019-08-17 10:59:02 -0400

    added basic auth

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
 const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
@@ -13,7 +15,32 @@ const app = express()
   .use(morgan("tiny"))
 
   // routes
-  .use(require("./routes"));
+  .use(require("./routes"))
+  .use(passport.initialize())
+  .use(passport.session())
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+});
+
+// Local Strategy for Passport
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
 
 // static files
 if (process.env.NODE_ENV === "production") {
