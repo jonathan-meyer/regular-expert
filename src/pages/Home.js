bf8a26b2e0198b72f5632189363d6a98fbd113e0
Anthony <anthony.cersosimo@gmail.com>
2019-08-17 13:26:49 -0400

    added the file structure and React pages into our src folder. This is jsut the basic template for our pages, still need to build out the react router so we can display the pages once the route is hit

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  state = {
+   message: "Hey, this is the home page!"
+  };
+
+
+  render() {
+    return (
+        <h1>{this.state.message}</h1>
+    );
+  }
+}
+
+export default Home;
