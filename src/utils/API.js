f313be7568a5b0f6336c892a56de38ed1562918b
Jonathan Meyer <jon@stej.com>
2019-08-20 13:52:34 -0400

    test React API

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 
-class API {
-  searchListings(query) {
-    return axios.get("/search", {
-      params: { q: query }
-    });
-  }
+const searchListings = query => {
+  return axios.get("/api/search", {
+    params: { q: query }
+  });
+};
 
-  listingDetails(property_id, listing_id) {
-    return axios.get(`/listing/${property_id}/${listing_id}`);
-  }
-}
+const listingDetails = (property_id, listing_id) => {
+  return axios.get(`/api/listing/${property_id}/${listing_id}`);
+};
 
-export default API;
+export default {
+  searchListings,
+  listingDetails
+};
