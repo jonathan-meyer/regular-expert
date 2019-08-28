048f78ac5865580a819d21c07862d4cd91fba403
Jonathan Meyer <jon@stej.com>
2019-08-27 20:53:00 -0400

    moved nav code from landing to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Group from "./pages/Group";
 import Groups from "./pages/Groups";
 import Landing from "./pages/Landing";
+
+import Navbar from "./components/Navbar";
+
 import API from "./utils/API";
 
 class App extends React.Component {
@@ -18,6 +22,7 @@ class App extends React.Component {
     return (
       <Router>
         <div>
+          <Navbar />
           <Switch>
             <Route exact path="/" component={Landing} />
             <Route exact path="/group" component={Group} />
