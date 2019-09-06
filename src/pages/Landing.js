30cd212a2bcbf9189f3957dd0e7daa5592a29e3d
amandanewton <47038978+amandanewton@users.noreply.github.com>
2019-09-05 21:31:05 -0400

    updated landing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import ReactPlayer from "react-player";
-
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 import ListGroup from "react-bootstrap/ListGroup";
 import Container from "react-bootstrap/Container";
 
@@ -41,10 +43,15 @@ class Landing extends Component {
   render() {
     const { query, listings, loading } = this.state;
 
+    const Mp4Style = {
+      margin: '0 auto',
+      paddingLeft: '0px',
+    }
+
     return (
-      <Card>
+      <Card style={Mp4Style}>
         <ReactPlayer
-          className="w-100"
+          class="card-img-underlay"
           width="100%"
           height="100%"
           url="https://player.vimeo.com/external/236075858.hd.mp4?s=539faad12f040eb5afd8de3160db1220f1a5bac0&profile_id=175&oauth2_token_id=57447761&download=1"
@@ -62,21 +69,35 @@ class Landing extends Component {
             }
           }}
         ></ReactPlayer>
-        <Card.ImgOverlay style={{ "margin-top": 490 }}>
+        <Card.ImgOverlay>
           <Card>
             <Card.Header>
-              <h1>Lets Find a home Together</h1>
+              <h1 className="text-center">Let's Find a Home Together</h1>
             </Card.Header>
             <Card.Body>
-              <Form onSubmit={e => this.handleFormSubmit(e)}>
-                <Form.Control
+              <Form.Label>
+              Looking for a new home? Want to collaborate with others on the properties you find interesting? Start your search now! 
+              </Form.Label>
+            
+            <Form onSubmit={e => this.handleFormSubmit(e)}>
+              <InputGroup className="mb-3">
+                <FormControl
+                  placeholder="Recipient's username"
+                  aria-label="Recipient's username"
+                  aria-describedby="basic-addon2"
                   location={this.state.location}
                   type="text"
                   id="query"
                   value={query}
                   onChange={e => this.changeHandler(e)}
-                ></Form.Control>
-              </Form>
+                />
+                <InputGroup.Append>
+                  <Button variant="dark">Find Home!</Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </Form>
+
+
               {loading && <span>loading...</span>}
               <ListGroup>
                 {listings.map((listing, key) => (
