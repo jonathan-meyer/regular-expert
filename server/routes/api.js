158a9aefe759487e7efc9fffe6cd35cabb179b60
Jonathan Meyer <jon@stej.com>
2019-09-08 23:08:22 -0400

    added new endpoint for getting listings of a group

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const _ = require("lodash");
 
 const crud = require("../../controllers/crud");
+const realtor = require("../../controllers/realtor");
+
 const User = require("../models/user");
+const Listing = require("../models/listing");
 
 const router = express.Router();
 
@@ -9,7 +13,47 @@ router.use("/realtor", require("./realtor"));
 
 router.use("/comment", crud(require("../models/comment")));
 router.use("/group", crud(require("../models/group")));
-router.use("/listing", crud(require("../models/listing")));
+
+router.use("/listing/group/:id", (req, res) => {
+  Listing.find({ group: req.params.id }, (err, listings) => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      Promise.all(
+        listings.map(listing => {
+          const { listing_id, property_id } = listing;
+
+          return realtor
+            .detail(property_id, listing_id)
+            .then(mls =>
+              _.omit(mls.listing, [
+                "tax_history",
+                "trend",
+                "property_history",
+                "price_history",
+                "photos",
+                "office",
+                "neighborhoods",
+                "neighborhood",
+                "mortgage",
+                "msl",
+                "lead_forms",
+                "client_provider_data",
+                "client_event_data",
+                "broker",
+                "client_display_flags",
+                "branding",
+                "agent"
+              ])
+            )
+            .then(mls => ({ ...listing.toObject(), mls }));
+        })
+      ).then(data => res.json(data));
+    }
+  });
+});
+
+router.use("/listing", crud(Listing));
 
 router.use("/user/list", (req, res) => {
   User.find({}, "_id firstName lastName", (err, result) => {
