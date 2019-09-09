41e842a868b8dcccc2aa1760db82f47f391e7a35
Anthony <anthony.cersosimo@gmail.com>
2019-09-08 20:58:19 -0400

    not really sure what's going on

--- src/components/Listing/index.js
+++ src/components/Listing/index.js
@@ -2,14 +2,18 @@ import React from "react";
 
 import ListGroup from "react-bootstrap/ListGroup";
 import Image from "react-bootstrap/Image";
+import { Button } from "react-bootstrap";
 
-export default function({ photo, address, price, beds, baths, sqft }) {
+export default function({ photo, address, price, beds, baths, sqft, property_id, listing_id }) {
   return (
     <ListGroup.Item>
       <Image src={photo} thumbnail width={240} className="float-left m-1" />
-      <p>{address}</p>
+      <h5 style={{fontStyle:'italic'}}>{address}</h5>
       <p>{price}</p>
       <p>{beds} beds | {baths} baths | {sqft}</p>
+      <Button
+        id='shareHome'
+        variant="primary">Share this Home</Button>
     </ListGroup.Item>
   );
 }
