80bd096ee1b12fbe7b7ce10c2c9799c9f5b7c841
Anthony <anthony.cersosimo@gmail.com>
2019-09-09 21:29:47 -0400

    updated API

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,15 +1,71 @@
 const express = require("express");
+const _ = require("lodash");
 
 const crud = require("../../controllers/crud");
+const realtor = require("../../controllers/realtor");
+
+const Group = require("../models/group");
 const User = require("../models/user");
+const Listing = require("../models/listing");
 
 const router = express.Router();
 
 router.use("/realtor", require("./realtor"));
 
 router.use("/comment", crud(require("../models/comment")));
-router.use("/group", crud(require("../models/group")));
-router.use("/listing", crud(require("../models/listing")));
+
+router.use("/group/mine", (req, res) => {
+  Group.find({ owner: req.user._id })
+    .populate("users")
+    .then(data => res.json(data))
+    .catch(err => {
+      console.log({ err });
+      res.sendStatus(404);
+    });
+});
+
+router.use("/group", crud(Group));
+
+router.use("/listing/group/:id", (req, res) => {
+  Listing.find({ group: req.params.id }, (err, listings) => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      Promise.all(
+        listings.map(listing => {
+          const { listing_id, property_id } = listing;
+
+          return realtor
+            .detail(property_id, listing_id)
+            .then(mls =>
+              _.omit(mls.listing, [
+                "tax_history",
+                "trend",
+                "property_history",
+                "price_history",
+                "photos",
+                "office",
+                "neighborhoods",
+                "neighborhood",
+                "mortgage",
+                "msl",
+                "lead_forms",
+                "client_provider_data",
+                "client_event_data",
+                "broker",
+                "client_display_flags",
+                "branding",
+                "agent"
+              ])
+            )
+            .then(mls => ({ ...listing.toObject(), mls }));
+        })
+      ).then(data => res.json(data));
+    }
+  });
+});
+
+router.use("/listing", crud(Listing));
 
 router.use("/user/list", (req, res) => {
   User.find({}, "_id firstName lastName", (err, result) => {
